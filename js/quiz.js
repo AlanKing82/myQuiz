6c84ca975834308c2c0e5df23b75e5b11c388013
Kasia Plezia <Kasia Plezia>
2016-05-02 17:59:36 +0100

    controller functions

--- js/quiz.js
+++ js/quiz.js
@@ -18,9 +18,38 @@
 
 	 	$scope.selectAnswer = function(qIndex, aIndex){
 	 		// alert(qIndex + ' ' + aIndex);
+            var questionState = $scope.myQuestions[qIndex].questionState;
+            
+            if(questionState != 'answered'){
+                $scope.myQuestions[qIndex].selectedAnswer = aIndex;
+                
+                // correct ans from json file
+                var correctAnswer = $scope.myQuestions[qIndex].correct;
+                
+                // set on scope object
+                $scope.myQuestions[qIndex].correctAnswer = correctAnswer;
+              
+                if(correctAnswer === aIndex){
+                    //$scope.myQuestions[qIndex].correctness = 'correct';
+                    $scope.score +=1;
+                }else{
+                   //$scope.myQuestions[qIndex].correctness = 'incorrect'; 
+                }
+                //questionState = 'answered';
+                 $scope.myQuestions[qIndex].questionState = 'answered';
+            }
 
 	 		
 	 	}
+        
+        // check functions fired in html(selectedAnswer,correctAnswer props added after click)
+        $scope.isSelected = function(qIndex, aIndex){
+            return $scope.myQuestions[qIndex].selectedAnswer === aIndex;
+        }
+        
+        $scope.isCorrect = function(qIndex, aIndex){
+            return $scope.myQuestions[qIndex].correctAnswer === aIndex;
+        }
 
 
 	 }]);
