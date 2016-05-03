d887686f6edcb5b49c9d4d8c734904f7e9b68abb
Alan King <alan.king@essentialskillz.com>
2016-05-03 18:07:30 +0100

    responsive etc

--- js/quiz.js
+++ js/quiz.js
@@ -30,14 +30,16 @@
                 $scope.myQuestions[qIndex].correctAnswer = correctAnswer;
               
                 if(correctAnswer === aIndex){
-                    //$scope.myQuestions[qIndex].correctness = 'correct';
+                    $scope.myQuestions[qIndex].correctness = 'correct';
                     $scope.score +=1;
                 }else{
-                   //$scope.myQuestions[qIndex].correctness = 'incorrect'; 
+                    $scope.myQuestions[qIndex].correctness = 'incorrect'; 
                 }
                 //questionState = 'answered';
                  $scope.myQuestions[qIndex].questionState = 'answered';
             }
+            
+            $scope.percentage = (($scope.score / $scope.totalQuestions) * 100).toFixed(1);
 
 	 		
 	 	}
@@ -49,6 +51,20 @@
         
         $scope.isCorrect = function(qIndex, aIndex){
             return $scope.myQuestions[qIndex].correctAnswer === aIndex;
+        }
+        
+        $scope.selectContinue = function(){
+            return $scope.activeQuestion +=1;
+        }
+        
+        $scope.createShareLinks = function(percentage){
+            var url = 'http://codifydesign.com';
+            
+            var emailLink = '<a class="btn email" href="mailto:?subject=Try to beat my quiz score!&amp;body=I scored '+percentage+'% on this quiz. Try to beat my score at '+url+'">Email a friend</a>';
+            var twitterLink = '<a class="btn twitter" href="http://twitter.com/share?text=I scored '+percentage+'% on this quiz. Try to beat my score at&amp;hashtags=Quiz&amp;url='+url+'" target="_blank">Tweet your score</a>';
+            var newMarkup = emailLink + twitterLink;
+            
+            return $sce.trustAsHtml(newMarkup);
         }
 
 
